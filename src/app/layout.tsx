8d57e505e0908049dda8d5b65283a47802579f07
lisboa-daniel <lisboa.daniel.dev@gmail.com>
2024-08-22 13:58:54 -0300

    feat: meta data splash image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,8 @@ const myFont = localFont({ src: [
 export const metadata: Metadata = {
   title: "Kolib - 2XKO Combo Translator tool",
   description: "K.O Library: Input your combos and see the translation, easier to visualize and send to friends!",
+  
+
 };
 
 export default function RootLayout({
@@ -23,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head >
+        <meta property="og:image" content={'/splash.png'} />
+      </head>
       <body className={myFont.className}>{children}</body>
     </html>
   );
